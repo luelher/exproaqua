3650c8dd899b4ddad1536c4e6bf4e320a3c984d3
luelher <luelher@32324e6b-4664-d1d6-1059-f55b3be2d140>
2010-04-13 01:27:05 +0000

    Formulario de reportes de produccion

---
+++ trunk/exproaqua/apps/frontend/modules/reportes/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/reportesGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/reportesGeneratorHelper.class.php';
+
+/**
+ * reportes actions.
+ *
+ * @package    exproaqua
+ * @subpackage reportes
+ * @author     Luis Hernández
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class reportesActions extends autoReportesActions
+{
+}
