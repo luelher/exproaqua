58c02261f330ea4a3407078b777f56b28691bb2b
luelher <luelher@32324e6b-4664-d1d6-1059-f55b3be2d140>
2010-04-13 01:26:27 +0000

    Formulario de reportes de produccion

---
+++ trunk/exproaqua/apps/frontend/modules/reportes/lib/reportesGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * reportes module configuration.
+ *
+ * @package    exproaqua
+ * @subpackage reportes
+ * @author     Luis Hernández
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class reportesGeneratorConfiguration extends BaseReportesGeneratorConfiguration
+{
+}
---
+++ trunk/exproaqua/apps/frontend/modules/reportes/lib/reportesGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * reportes module helper.
+ *
+ * @package    exproaqua
+ * @subpackage reportes
+ * @author     Luis Hernández
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class reportesGeneratorHelper extends BaseReportesGeneratorHelper
+{
+}
