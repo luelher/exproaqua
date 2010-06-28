0a4e1789f63e120c56fce29a01ae7878ec7743f1
luelher <luelher@32324e6b-4664-d1d6-1059-f55b3be2d140>
2010-06-28 16:07:54 +0000

    Version para prueba con la integracion con premium

---
+++ exproaqua/apps/frontend/modules/informenes/actions/actions.class.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/informenesGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/informenesGeneratorHelper.class.php';
+
+/**
+ * informenes actions.
+ *
+ * @package    exproaqua
+ * @subpackage informenes
+ * @author     Luis Hernández
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class informenesActions extends autoInformenesActions
+{
+  public function executeEdit(sfWebRequest $request) {
+    $this->redirect('@ord_pro_informenes');
+  }
+
+
+  public function executeInforme(sfWebRequest $request)
+  {
+    $config = sfTCPDFPluginConfigHandler::loadConfig();
+
+    // pdf object
+    $pdf = new sfTCPDF();
+
+    // settings
+    $pdf->SetFont('FreeSerif', '', 12);
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->setHeaderFont(array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+    $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+    $pdf->setFooterFont(array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+    // init pdf doc
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->Ln();
+    $pdf->Ln();
+    $pdf->Cell(40,20,'Hola Mundo');
+
+    // output
+    $pdf->Output();
+
+    // Stop symfony process
+    throw new sfStopException();
+  }
+
+}
